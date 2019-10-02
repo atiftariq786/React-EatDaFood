35634dcf4bfb7446a7699766e7c908d500398b79
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-02 16:34:01 -0700

    Component: Burger and burgerIngredient added

--- eat-da-food/src/components/Layout/Layout.js
+++ eat-da-food/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import styles from "./layout.module.css";
 
 const layout = (props) => (
 
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className = {styles.content}>
             {props.children}
         </main>
     
