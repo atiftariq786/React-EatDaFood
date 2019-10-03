92bc5cd16d83dda8ca1dbf527b0857acdf3399bd
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-02 17:02:48 -0700

    Component:BburgerIngredient switch case added

--- eat-da-food/src/components/Burger/BurgerIngredient/BurgerIngredien.js
+++ eat-da-food/src/components/Burger/BurgerIngredient/BurgerIngredien.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, {Component} from "react";
+import styles from "./BurgerIngredient.module.css";
+import PropTypes from "prop-types";
+import BurgerBuilder from "../../../containers/BurgerBuilder/BurgerBuilder";
 
 
-const burgerIngredient = (props) =>{
+class BurgerIngredient extends Component{
 
+    render(){
+        let ingredient = null;
+
+    switch(this.props.type){
+        case ("bread-bottom"): 
+            ingredient = <div className={styles.BreadBottom}></div>
+            break;
+        case ("bread-top"):
+            ingredient = (
+                <div className={styles.BreadTop}>
+                    <div className={styles.Seeds1}></div>
+                    <div className={styles.Seeds2}></div>
+                </div>
+            );
+            break;
+        case ("meat"):
+            ingredient = <div className ={styles.Meat}></div>
+            break;
+        case ("chees"):
+            ingredient = <div className ={styles.Cheese}></div>
+            break;
+        case ("bacon"):
+            ingredient = <div className ={styles.Bacon}></div>
+            break;
+        case ("salad"):
+            ingredient = <div className ={styles.Salad}></div>
+            break;
+            default:
+                ingredient = null;
+    }
+    return ingredient;
+
+        
+        
+    }
+}
+BurgerBuilder.propTypes = {
+    type: PropTypes.string.isRequired
 };
 
-export default burgerIngredient;
+
+export default BurgerIngredient;
