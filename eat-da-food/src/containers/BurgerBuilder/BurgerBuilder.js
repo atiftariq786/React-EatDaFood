dfbdc37267bfecc3a81bdca26b7a4b6682e0c6db
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-03 11:28:03 -0700

    Container: Addingredient code in burger builder

--- eat-da-food/src/containers/BurgerBuilder/BurgerBuilder.js
+++ eat-da-food/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,6 +23,24 @@ class BurgerBuilder extends Component {
        
     };
     
+    addingIngredientHandler = (type) =>{
+
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount +1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type]=updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({
+            totalPrice : newPrice,
+            ingredients : updatedIngredients
+        })
+
+    }
+    
     render(){
         const disabledInfo = {
             ...this.state.ingredients
