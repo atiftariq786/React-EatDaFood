00e128100504663d660586c16ae74c0f5a42bf9f
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-02 20:38:27 -0700

    Component:Bburger styling update and code added

---
+++ eat-da-food/src/components/Burger/Burger.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Burger.module.css";
+
+const burger = (props) => {
+
+    return(
+        <div className = {styles.Burger}></div>
+
+    );
+};
+export default burger;
