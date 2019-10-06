989fdb70e0e575aa2f1e61c4b3120ff73e7f4820
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-06 16:38:59 -0700

    Component/Logo: Set in toolbar

--- eat-da-food/src/components/Logo/Logo.js
+++ eat-da-food/src/components/Logo/Logo.js
@@ -1,8 +1,10 @@
 import React from "react";
+import burgerLogo from "../../assets/images/burger-logo.png";
+import styles from "./Logo.module.css";
 
 const logo = (props) => (
-    <div>
-        <img src="../../assets/images/burger-logo.png"></img>
+    <div className={styles.Logo}>
+        <img src={burgerLogo} alt="MyBurger"></img>
     </div>
 
 );
