21cc485a7836aa40c0cd0ad9b7c84ae31ed75686
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-06 16:10:24 -0700

    Component/Navigation:Toolbarcss code added and styling set

--- eat-da-food/src/components/Navigation/Toolbar/Toolbar.js
+++ eat-da-food/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./Toolbar.module.css";
 
 const toolbar = (props) => (
-<div></div>
+    <header className={styles.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
 );
 export default toolbar;
 
+ 
