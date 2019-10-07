48381cbbc70c3ba05ecbb4658322a88092fa7da2
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-06 22:26:17 -0700

    Menu icon added and navigation bug fixed

--- eat-da-food/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ eat-da-food/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,6 +1,11 @@
 import React from "react";
+import styles from "./DrawerToggle.module.css";
 
 const drawerToggle = (props)=>(
-    <div onClick={props.clicked}>MENU</div>
+    <div className={styles.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>    
+    </div>
 );
 export default drawerToggle;
