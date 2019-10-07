d7ec1249fd759847676ac8d63f22f6cf56403714
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-06 18:07:46 -0700

    Component/Navigation: SideDrawer component added and code added

---
+++ eat-da-food/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+const sideDrawer = (props)=>{
+
+
+    return (
+        <div>
+            <Logo/>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </div>
+
+    )
+}
+export default sideDrawer;
