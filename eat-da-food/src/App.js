867d81388c32550a4caac918f09404261eadfdd5
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-11 11:36:54 -0700

    App.js routing configuration using switch and route

--- eat-da-food/src/App.js
+++ eat-da-food/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import {Route, Switch} from "react-router-dom;"
 
 class App extends Component {
 
@@ -10,12 +11,11 @@ class App extends Component {
     return (
       <div>
       <Layout>
-        <BurgerBuilder />
-        <Checkout />
-      
-      
-      </Layout>
-        
+        <Switch>  
+          <Route path="/checkout" component={Checkout} /> 
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>      
+      </Layout>        
       </div>
     );
   } 
