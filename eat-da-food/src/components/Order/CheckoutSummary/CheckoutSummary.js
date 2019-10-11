fe0ab4e832ffd6115b49ca9990ae9b505e0f3584
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-10 23:30:50 -0700

    Component: Inside order comp checkoutsummary code chunk added

--- eat-da-food/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ eat-da-food/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./CheckoutSummary.module.css";
+import Burger from "../../Burger/Burger";
 
 const checkoutSummary = (props) => {
 
+    return (
+        <div>
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: "300px", height:"300px", margin:"auto"}}></div>
+            <Burger />
+            </div>
+    )
 
 }
 export default checkoutSummary;
