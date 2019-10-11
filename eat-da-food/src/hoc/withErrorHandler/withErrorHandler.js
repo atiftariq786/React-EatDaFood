fd92510b7659806fb7c444f9ecb32eb20da004c6
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-10 23:21:06 -0700

    Container: checkout component added

--- eat-da-food/src/hoc/withErrorHandler/withErrorHandler.js
+++ eat-da-food/src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,13 +9,17 @@ const withErrorHandler = (WrappedComponent, axios) =>{
             error: null
         }
         componentWillMount () {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error : null});
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.reqInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error : error})
             });
+        }
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
         errorConfirmedHandler =() =>{
             this.setState({error: null})
