3ccee37899925188598c0f4f2bfbc74770e1433a
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-11 15:24:19 -0700

    Container: checkout.js query param added in componentDidMount to passing ingredients to checkout page

--- eat-da-food/src/containers/BurgerBuilder/BurgerBuilder.js
+++ eat-da-food/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -80,7 +80,15 @@ class BurgerBuilder extends Component {
             //console.log(error));
         });
         */
-       this.props.history.push("/checkout");
+       const queryParams = [];
+       for (let i in this.state.ingredients){
+           queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
+       }
+       const queryString = queryParams.join("&");
+       this.props.history.push({
+           pathname:"/checkout",
+           search:"?" + queryString
+        });
         
     }
     
