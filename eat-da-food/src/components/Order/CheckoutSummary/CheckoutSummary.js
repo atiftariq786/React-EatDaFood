e0aadf39e573bc1578495e6079ec89dc77516bf5
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-10 23:36:27 -0700

    Component: Inside checkout button added

--- eat-da-food/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ eat-da-food/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,15 +1,19 @@
 import React from "react";
 import styles from "./CheckoutSummary.module.css";
 import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
 
 const checkoutSummary = (props) => {
 
     return (
         <div>
             <h1>We hope it tastes well!</h1>
-            <div style={{width: "300px", height:"300px", margin:"auto"}}></div>
-            <Burger />
+            <div style={{width: "300px", height:"300px", margin:"auto"}}>
+                <Burger ingredient = {props.ingredients} />
             </div>
+            <Button btnType = "Danger">CANCEL</Button>  
+            <Button btnType = "Success">CONTINUE</Button>  
+        </div>
     )
 
 }
