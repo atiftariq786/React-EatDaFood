936b16c8af891e936c1798198fbf01db05f44743
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-12 19:06:14 -0700

    Container: Fetch the data from the firebase

--- eat-da-food/src/containers/Orders/Orders.js
+++ eat-da-food/src/containers/Orders/Orders.js
@@ -1,14 +1,40 @@
 import React, {Component} from "react";
 import Order from "../../components/Order/Order";
+import axios from "../../axios";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+
+    state={
+        orders:[],
+        loading:true
+    }
+
+    componentDidMount(){
+        axios.get("orders.json")
+        .then(res => {
+            console.log(res.data);
+            let fetchedOrders = [];
+
+            for( let key in res.data){
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id:key
+                });
+            }
+            this.setState({loading:false});
+        })
+        .catch(err => {
+            this.setState({loading:false});
+        });
+    }
     render(){
         return(
             <div>
-            <Order />
-            <Order />
+           
+            
             </div>
         )
     }
 }
-export default Orders;
+export default withErrorHandler(Orders, axios);
