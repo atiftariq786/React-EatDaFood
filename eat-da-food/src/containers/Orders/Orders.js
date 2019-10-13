ce508be193baee6feb7f22785618aff65c6b79aa
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-12 17:46:58 -0700

    Container: Order import and set in Orders component

--- eat-da-food/src/containers/Orders/Orders.js
+++ eat-da-food/src/containers/Orders/Orders.js
@@ -1,9 +1,12 @@
 import React, {Component} from "react";
+import Order from "../../components/Order/Order";
 
 class Orders extends Component {
     render(){
         return(
             <div>
+            <Order />
+            <Order />
             </div>
         )
     }
