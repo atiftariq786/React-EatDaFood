9857f05f8a4e75e13a4c3d51f1f9eeaff23925d1
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-12 19:45:40 -0700

    Container: Fetched data ingredient outputting showed

--- eat-da-food/src/components/Order/Order.js
+++ eat-da-food/src/components/Order/Order.js
@@ -12,11 +12,22 @@ const order = (props)=> {
             amount: props.ingredients[ingredientName]
         });
     }
-    
+    const ingredientOutput = ingredients.map(ig => {
+        return <span 
+        style ={{
+            textTransform: "capitalize",
+            display: "inline-block",
+            margin: "0 8px",
+            border: "1px solid #ccc",
+            padding:  "5px"
+        }}
+        key={ig.name}
+        >{ig.name} ({ig.amount}) </span>
+    });
     
     return(
     <div className={styles.Order}>
-        <p>Ingredients:  </p>
+        <p>Ingredients: {ingredientOutput}  </p>
         <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
 );
