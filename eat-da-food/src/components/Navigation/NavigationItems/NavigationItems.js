cc9e430de0738d9d727f7d679bcc0e95c5ea84f0
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-12 18:05:55 -0700

    Component: Navigation link update

--- eat-da-food/src/components/Navigation/NavigationItems/NavigationItems.js
+++ eat-da-food/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = (props) => (
     <ul className={styles.NavigationItems}>
-        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 );
