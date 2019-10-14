5d8ad827fa6cef9e5d797e49d2ffb613d9e39cdf
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-14 16:37:53 -0700

    Component/UI: input component added and code chunk added in this file

---
+++ eat-da-food/src/components/UI/Input/Input.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Input.module.css";
+
+const input = (props) =>{
+
+    return (
+        <div>
+        
+        </div>
+    )
+}
+export default input;
