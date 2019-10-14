748f2923274d91240282e288846a1d8fe98137cd
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-14 16:50:08 -0700

    Component/UI: input component added and switch method added

--- eat-da-food/src/components/UI/Input/Input.js
+++ eat-da-food/src/components/UI/Input/Input.js
@@ -2,9 +2,16 @@ import React from "react";
 import styles from "./Input.module.css";
 
 const input = (props) =>{
+    let inputElement = null;
+
+    switch(props.inputType){
+        case("input"): inputElement = <input /> 
+        case("textarea"): inputElement = <textarea /> 
+    }
 
     return (
         <div>
+            <label>{props.label}</label>
         
         </div>
     )
