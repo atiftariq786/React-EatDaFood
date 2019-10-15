8303a532c5be77535926dca6fead98f0216f07c9
Qandeel Sajid <atiftariq786@hotmail.com>
2019-10-14 17:21:20 -0700

    Component/UI: input component style update

--- eat-da-food/src/components/UI/Input/Input.js
+++ eat-da-food/src/components/UI/Input/Input.js
@@ -5,13 +5,18 @@ const input = (props) =>{
     let inputElement = null;
 
     switch(props.inputType){
-        case("input"): inputElement = <input /> 
-        case("textarea"): inputElement = <textarea /> 
+        case("input"): inputElement = <input {...props} />;
+            break; 
+        case("textarea"): inputElement = <textarea {...props} />;
+            break;
+        default:
+            inputElement=<input {...props}/>
     }
 
     return (
         <div>
             <label>{props.label}</label>
+            {inputElement}
         
         </div>
     )
