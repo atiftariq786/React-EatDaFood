e919831497bbcafe952f47dd6956f7f24c172855
Qandeel Sajid <atiftariq786@hotmail.com>
2019-11-04 16:39:02 -0800

    BurgerBuilder.js update

--- eat-da-food/src/index.js
+++ eat-da-food/src/index.js
@@ -4,7 +4,6 @@ import {BrowserRouter} from "react-router-dom";
 import { Provider }  from "react-redux";
 import { createStore } from "redux";
 
-
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
--- eat-da-food/src/store/reducer.js
+++ eat-da-food/src/store/reducer.js
@@ -1,6 +1,5 @@
 import * as actionTypes from "./actions";
 
-
 const initialState ={
     ingredients :{
         salad: 0,
